ec6391fe26cc3aca0367a2a1bf0689cd4c604395
santiagocity515 <santiago20gta5@gmail.com>
2024-03-30 01:02:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Viendolos 👀","Creador: Jesucristo_0"];
 
 
 let currentIndex = 0;
